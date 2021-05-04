ff2a7ec8e515bf9433b37df06503e0fc6328e499
EphMi <65047304+EphMi@users.noreply.github.com>
2021-05-04 11:28:27 +0000

    Add files via upload

---
+++ static/nifann.means_transport.js
@@ -0,0 +1,46 @@
+
+/** -----------------------------------------------------------------------------
+ *  File name         : nifann.means_transport.js
+ *  Begin             : 03-05-2021
+ *  Last_Update       : 03-05-2021
+ * 
+ *  Description       : Nifann Means Transport
+ * 
+ *  Author            : Eph & Mi 
+ *  Contact           :
+ *                         <m.k@ephmi.com>
+ *                         <support@ephmi.com>
+ *
+ * (c) Copyright 2021 Eph & Mi
+ * ---------------------------------------------------------------------------  */
+
+ [
+  {
+    'code':'VELO',
+    'explaining':'A Bike is suitable to used to send the package.'
+  },
+  {
+    'code':'MOTO',
+    'explaining':'A Moto is suitable to send the package.'
+  },
+  {
+    'code':'TRICYLE',
+    'explaining':'A Tricycle is suitable to send the package.'
+  },
+  {
+    'code':'VOITURE',
+    'explaining':'A Car is suitable to send the package.'
+  },
+  {
+    'code':'CAMION',
+    'explaining':'A Truck is suitable to send the package.'
+  },
+  {
+    'code':'BATEAU',
+    'explaining':'A Boat is suitable to send the package.'
+  },
+  {
+    'code':'TRAIN',
+    'explaining':'A Train is suitable to send the package.'
+  },
+];
