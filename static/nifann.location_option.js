4c519eabdec79909decb8998aa5537d7c33cf0ce
EphMi <65047304+EphMi@users.noreply.github.com>
2021-05-04 08:31:47 +0000

    Add files via upload

---
+++ static/nifann.location_option.js
@@ -0,0 +1,36 @@
+
+/** -----------------------------------------------------------------------------
+ *  File name         : nifann.location_option.js
+ *  Begin             : 03-05-2021
+ *  Last_Update       : 03-05-2021
+ * 
+ *  Description       : Deliveries options 
+ * 
+ *  Author            : Eph & Mi 
+ *  Contact           :
+ *                         <m.k@ephmi.com>
+ *                         <support@ephmi.com>
+ *
+ * (c) Copyright 2021 Eph & Mi
+ * ---------------------------------------------------------------------------  */
+
+
+ [
+   {
+     'code':'1',
+     'name':'MYCOUNTRY_MYCITY',
+     'explaining':'The package must be delivered within my city.'
+   },
+   {
+     'code':'2',
+     'name':'MYCOUNTRY_OTHERCITY',
+     'explaining':'The package must be delivered in other city, different of my current city.'
+   },
+   {
+     'code':'3',
+     'name':'OTHER_COUNTRY',
+     'explaining':'The package must be delivered in other country, different of my current country.'
+   },
+  ]
+
+ 
